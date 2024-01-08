723e4e456379240b0bc818f34b50162c32f5172a
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-08 06:41:24 +0000

    C problems practice

---
+++ day-one.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Problem Questions 01 : Write a program to calculate the area of a rectangle.
+    /*int length, width, area;
+     printf("\nPlease enter all area size: ");
+     scanf("%d %d", &length, &width);
+     area = length * width;
+     printf("The area of Rectungular is : %d\n", area);
+     */
+
+    // Problem Questions 02 : Write a program to calculate the Perimeter of a rectangle.
+    /*int length, width, perimeter;
+     printf("\nPlease enter width and length: ");
+     scanf("%d %d", &length, &width);
+     perimeter = 2 * (width + length);
+     printf("The peremeter of Rectungular is : %d\n", perimeter);
+     */
+
+    // Problem Questions 03 : Create a program to find the sum and average of three numbers.
+    /*int numOne,numTwo,numThree,sum,average;
+    printf("\nPlease enter three number as Input: ");
+    scanf("%d %d %d", &numOne, &numTwo, &numThree);
+    sum = numOne+numTwo+numThree;
+    average = (numOne+numTwo+numThree)/3;
+    printf("\nSum : %d\n", sum);
+    printf("Average : %d\n", average);
+    */
+
+    // Problem Questions 04: Develop a simple calculator program that can perform addition, subtraction, multiplication, and division.
+    /*int n, sum = 0, subtract, multiply, division;
+    int numStore[n];
+    printf("\nPlease enter what number you want calculate: ");
+    scanf("%d", &n);
+    int numInput[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &numInput[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + numInput[i];
+        subtract = subtract-numInput[i];
+        multiply = multiply*numInput[i];
+        division = division/numInput[i];
+    }
+    printf("\n%d\n", sum);
+    printf("\n%d\n", subtract);
+    printf("\n%d\n", multiply);
+    printf("\n%d\n", division);
+    */
+
+   //Problem Questions 05 : Write a program to check if a number is even or odd.
+   /*int numOne=11, even,odd;
+   if(numOne%2==0){
+    printf("Numone even");
+   }else{
+    printf("Odd");
+   }
+   */
+}
